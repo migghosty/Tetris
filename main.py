c79eb7a42667a4f5d7a1a371f47cbbaee4653326
migghosty <miguel6886@gmail.com>
2021-11-08 14:26:13 -0800

    Added it so it spawns a random tetris piece at the top, and can also rotate the piece in motion.

--- main.py
+++ main.py
@@ -4,7 +4,112 @@ import random
 import const
 
 BLOCK_TYPES = ['i', 'j', 'l', 'o', 's', 't', 'z']
+BLOCK_SHAPES = [
+    # block i
+    [
+        [[0,0,0,0],
+         [1,1,1,1],
+         [0,0,0,0],
+         [0,0,0,0]],
+        [[0,0,1,0],
+         [0,0,1,0],
+         [0,0,1,0],
+         [0,0,1,0]]
+    ],
+    # block j
+    [
+        [[0,0,0,0],
+         [1,0,0,0],
+         [1,1,1,0],
+         [0,0,0,0]],
+        [[0,0,1,1],
+         [0,0,1,0],
+         [0,0,1,0],
+         [0,0,0,0]],
+        [[0,0,0,0],
+         [1,1,1,0],
+         [0,0,1,0],
+         [0,0,0,0]],
+        [[0,1,0,0],
+         [0,1,0,0],
+         [1,1,0,0],
+         [0,0,0,0]]
+    ],
+    # block l
+    [
+        [[0,0,0,0],
+         [0,0,0,1],
+         [0,1,1,1],
+         [0,0,0,0]],
+        [[0,0,1,0],
+         [0,0,1,0],
+         [0,0,1,1],
+         [0,0,0,0]],
+        [[0,0,0,0],
+         [1,1,1,0],
+         [1,0,0,0],
+         [0,0,0,0]],
+        [[1,1,0,0],
+         [0,1,0,0],
+         [0,1,0,0],
+         [0,0,0,0]]
+    ],
+    # block o
+    [
+        [[0,0,0,0],
+         [0,1,1,0],
+         [0,1,1,0],
+         [0,0,0,0]]
+    ],
+    # block s
+    [
+        [[0,0,0,0],
+         [0,1,1,0],
+         [1,1,0,0],
+         [0,0,0,0]],
+        [[1,0,0,0],
+         [1,1,0,0],
+         [0,1,0,0],
+         [0,0,0,0]]
+    ],
+    # block t
+    [
+        [[0,0,0,0],
+         [0,1,0,0],
+         [1,1,1,0],
+         [0,0,0,0]],
+        [[0,1,0,0],
+         [0,1,1,0],
+         [0,1,0,0],
+         [0,0,0,0]],
+        [[0,0,0,0],
+         [1,1,1,0],
+         [0,1,0,0],
+         [0,0,0,0]],
+        [[0,1,0,0],
+         [1,1,0,0],
+         [0,1,0,0],
+         [0,0,0,0]]
+    ],
+    # block z
+    [
+        [[0,0,0,0],
+         [0,1,1,0],
+         [0,0,1,1],
+         [0,0,0,0]],
+        [[0,0,0,1],
+         [0,0,1,1],
+         [0,0,1,0],
+         [0,0,0,0]]
+    ]
+]
 
+class Block:
+    def __init__(self, type: str, rot: int, x: int, y: int):
+        self.type = type
+        self.rot = rot
+        self.x = x
+        self.y = y
 
 class TetrisGame:
     def __init__(self):
@@ -15,6 +120,8 @@ class TetrisGame:
 
         # make the grid
         self._grid = [[0 for _ in range(const.BOARD_WIDTH)] for _ in range(const.BOARD_HEIGHT)]
+
+        self._block_in_motion = self._get_random_block()
 
     def _initialize_pygame(self):
         # initialize pygame modules
@@ -37,32 +144,65 @@ class TetrisGame:
     def _convert_board_coordinate(self, x: int, y: int) -> (int, int):
         return ( x*const.BLOCK_SIZE + const.TOP_LEFT[0], y*const.BLOCK_SIZE + const.TOP_LEFT[1] )
 
-    def _draw_border_block(self, x: int, y: int) -> None:
+    def _draw_block(self, x: int, y: int, img_path: str) -> None:
         # draw a single block with a given color at a given (x,y) position
         # the (x,y) position is given should be based of the tetris board, not pixels.
         # for example, the block to the right of (0,0) is (1,0)
-        img = pygame.image.load("imgs/border_block.png").convert()
+        img = pygame.image.load(img_path).convert()
         img = pygame.transform.scale(img, (const.BLOCK_SIZE, const.BLOCK_SIZE))
         self._screen.blit(img, self._convert_board_coordinate(x,y))
+
+    def _erase_block(self, x: int, y: int) -> None:
+        # erase the block so it matches background
+        img = pygame.image.load("imgs/background.png").convert()
+        img = pygame.transform.scale(img, (const.BLOCK_SIZE, const.BLOCK_SIZE))
+        self._screen.blit(img, self._convert_board_coordinate(x, y))
+
+    def _erase_block_in_motion(self) -> None:
+        shape_grid = BLOCK_SHAPES[BLOCK_TYPES.index(self._block_in_motion.type)][self._block_in_motion.rot]
+        for i in range(len(shape_grid)):
+            for j in range(len(shape_grid[i])):
+                if shape_grid[j][i] == 1:
+                    self._erase_block(self._block_in_motion.x+1+i,self._block_in_motion.y+1+j)
 
     def draw_border(self) -> None:
         # draws the top and bottom row
         for i in range(const.BOARD_WIDTH):
-            self._draw_border_block(i, 0)
-            self._draw_border_block(i, const.BOARD_HEIGHT - 1)
+            self._draw_block(i, 0, "imgs/border_block.png")
+            self._draw_block(i, const.BOARD_HEIGHT - 1, "imgs/border_block.png")
         # draws the side columns
         for i in range(const.BOARD_HEIGHT):
-            self._draw_border_block(0, i)
-            self._draw_border_block(const.BOARD_WIDTH-1, i)
+            self._draw_block(0, i, "imgs/border_block.png")
+            self._draw_block(const.BOARD_WIDTH-1, i, "imgs/border_block.png")
+
+    def draw_shape(self, block: Block) -> None:
+        shape_grid = BLOCK_SHAPES[BLOCK_TYPES.index(block.type)][block.rot]
+        for i in range(len(shape_grid)):
+            for j in range(len(shape_grid[i])):
+                if shape_grid[j][i] == 1:
+                    self._draw_block(block.x+1+i,block.y+1+j,f"imgs/{block.type}_block.png")
+
+    def rotate_shape(self, block: Block) -> None:
+        block.rot = (block.rot + 1) % len(BLOCK_SHAPES[BLOCK_TYPES.index(block.type)])
+
+    def _get_random_block(self) -> Block:
+        return Block(random.choice(BLOCK_TYPES),0,const.BOARD_WIDTH/2-3,0)
 
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    # erase the old shape and rotate, then redraw it
+                    self._erase_block_in_motion()
+                    self.rotate_shape(self._block_in_motion)
+
     def run(self):
         while True:
             self.draw_border()
+            self.draw_shape(self._block_in_motion)
             self.handle_events()
             pygame.display.flip()
 
